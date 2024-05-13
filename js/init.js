b785dcc6b4c356b93ade11de98a31e9b82670db8
albertkun <albertk@gmx.com>
2024-05-13 00:46:04 -0700

    refactor: menu and about modal in index.html and init.js

--- js/init.js
+++ js/init.js
@@ -154,3 +154,22 @@ resetButton.addEventListener('click', function() {
 });
 
 
+
+let modal = document.getElementById("aboutModal");
+let btn = document.querySelector("#menu a[href='#about']");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
